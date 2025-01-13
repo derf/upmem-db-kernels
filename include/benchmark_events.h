3f686cb5d7f411425b0946b954d1aa591ebf16a8
Birte Kristina Friesel <birte.friesel@uos.de>
2025-01-13 10:26:11 +0100

    Get benchmark events from a separate include

---
+++ include/benchmark_events.h
@@ -0,0 +1,7 @@
+struct benchmark_event benchmark_events[] = {
+{op_select, pred_lt, 10864299394},
+{op_select, pred_ne, 14655859972},
+{op_select, pred_ne, 13349417872},
+{op_select, pred_ne, 8785524908},
+{op_select, pred_eq, 9713414926},
+};
