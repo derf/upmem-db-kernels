7a11fc5281f431e86b7242458219e8a9fe0e53a7
Birte Kristina Friesel <birte.friesel@uos.de>
2025-01-16 14:12:57 +0100

    nodmc25.sh: benchmark order

--- nodmc25.sh
+++ nodmc25.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source /opt/upmem/upmem-2024.2.0-Linux-x86_64/upmem_env.sh
+git checkout include/benchmark_events.h
 make -B tinos=1
 
 mkdir -p log/$(hostname)
@@ -30,6 +31,19 @@ parallel -j1 --eta --joblog ${fn}.node1.joblog --resume --header : \
 	::: n_threads 1 2 4 6 8 \
 	::: i 1 2 3 4 5 >> ${fn}.node1.txt
 
+fn=log/$(hostname)/nodmc25-upmem
+
+parallel -j1 --eta --joblog ${fn}.node1.joblog --resume --header : \
+	run_benchmark_nmc numa_mem={numa_mem} numa_cores={numa_cores} n_elements={n_elements} n_threads={n_threads} n_ranks={n_ranks} \
+	::: numa_mem 1 \
+	::: numa_cores 8-15 \
+	::: n_elements $((1024*1024)) $((1024*1024*2)) $((1024*1024*4)) $((1024*1024*8)) $((1024*1024*16)) $((1024*1024*32)) $((1024*1024*64)) $((1024*1024*128)) $((1024*1024*256)) $((1024*1024*512)) $((1024*1024*1024)) $((1024*1024*1024*2)) $((1024*1024*1024*3)) $((1024*1024*1024*4)) $((1024*1024*1024*5)) $((1024*1024*1024*6)) $((1024*1024*1024*7)) \
+	::: n_threads 1 2 4 6 8 \
+	::: n_ranks 1 2 4 8 12 16 20 24 28 32 36 40 \
+	::: i 1 2 3 4 5 >> ${fn}.node1.txt
+
+fn=log/$(hostname)/nodmc25-cpu
+
 parallel -j1 --eta --joblog ${fn}.node0.joblog --resume --header : \
 	run_benchmark_cpu numa_mem={numa_mem} numa_cores={numa_cores} n_elements={n_elements} n_threads={n_threads} \
 	::: numa_mem 0 \
@@ -48,12 +62,3 @@ parallel -j1 --eta --joblog ${fn}.node0.joblog --resume --header : \
 	::: n_threads 1 2 4 6 8 \
 	::: n_ranks 1 2 4 8 12 16 20 24 28 32 36 40 \
 	::: i 1 2 3 4 5 >> ${fn}.node0.txt
-
-parallel -j1 --eta --joblog ${fn}.node1.joblog --resume --header : \
-	run_benchmark_nmc numa_mem={numa_mem} numa_cores={numa_cores} n_elements={n_elements} n_threads={n_threads} n_ranks={n_ranks} \
-	::: numa_mem 1 \
-	::: numa_cores 8-15 \
-	::: n_elements $((1024*1024)) $((1024*1024*2)) $((1024*1024*4)) $((1024*1024*8)) $((1024*1024*16)) $((1024*1024*32)) $((1024*1024*64)) $((1024*1024*128)) $((1024*1024*256)) $((1024*1024*512)) $((1024*1024*1024)) $((1024*1024*1024*2)) $((1024*1024*1024*3)) $((1024*1024*1024*4)) $((1024*1024*1024*5)) $((1024*1024*1024*6)) $((1024*1024*1024*7)) \
-	::: n_threads 1 2 4 6 8 \
-	::: n_ranks 1 2 4 8 12 16 20 24 28 32 36 40 \
-	::: i 1 2 3 4 5 >> ${fn}.node1.txt
